0e4e41be14fef688278c95399c652d1c438dd76e
adarvin14 <amdarvin14@gmail.com>
2020-12-06 16:27:23 -0500

    create routes for devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {omniauth_callbacks: 'omniauth'}
+    root to: 'application#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  devise_scope :user do
+    get '/users/sign_in', to: 'devise/sessions#new'
+    post '/users/sign_in', to: 'devise/sessions#create'
+    get '/users/sign_up', to: 'devise/sessions#new'
+    post '/users/sign_up', to: 'devise/sessions#create'
+    get '/users/sign_out', to: 'devise/sessions#destroy'
+  end
+
+  get '/users/:id', to: 'users#show'
+
+  get ':not_found' => redirect('/'), :constraints => { :not_found => /.*/ }
 end
