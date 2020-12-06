288037b03478638d993ae49244733ad2938d2a5c
adarvin14 <amdarvin14@gmail.com>
2020-12-06 16:45:20 -0500

    omniauth migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   has_many :genres, through: :songs
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable, :omniauthable,
          :recoverable, :rememberable, :validatable
 
   def self.from_omniauth(auth)
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -272,6 +272,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
+  config.omniauth :github, 'dac5cc69059220ad10c5', '87976ddc8988f5057d91b480a5dbd509cc6199b1', scope: 'user:email'
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
