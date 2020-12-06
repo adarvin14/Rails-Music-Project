0ec451d57449ed827e4316aec3890cfe81e54c16
adarvin14 <amdarvin14@gmail.com>
2020-12-06 16:08:11 -0500

    devise setup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "Rails_Music_Project_production"
+  # config.active_job.queue_name_prefix = "rails_sports_message_board_production"
 
   config.action_mailer.perform_caching = false
 
